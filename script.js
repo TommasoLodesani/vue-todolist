49f803872fb3d673de4871292b937b2675925f18
Tommaso <tommaso.lodesani@gmail.com>
2022-05-26 15:24:18 +0200

    add li item and text decoration

---
+++ script.js
@@ -0,0 +1,25 @@
+const myApp = new Vue({
+    el : "#app",
+    data : {
+        todos : [
+
+            {
+                text : "Fare i compiti",
+                done : false
+                
+            },
+            {
+                text : "Fare la lavabucato",
+                done : true
+                
+            },
+            {
+                text : "Stendere",
+                done : false
+                
+            },
+
+        ]
+    }    
+   
+});
